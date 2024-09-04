75f372abfa36990212f2dadb4692f99ae0083504
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 21:15:28 +0600

    accrodian completed

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        question : "lorem1",
+        answer : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ducimus fugiat impedit debitis, accusamus aperiam culpa adipisci temporibus excepturi, obcaecati consequuntur quos quibusdam optio iure ullam amet ipsum inventore rerum voluptate corrupti ipsa consectetur voluptas vero molestias! Placeat consequuntur voluptate excepturi, id veniam laudantium neque, distinctio porro et voluptatibus repellendus! Accusamus assumenda deleniti corporis. Ipsam quae amet illo numquam veritatis at fugiat incidunt mollitia, doloremque velit distinctio suscipit sequi laudantium minus repellat voluptatem nostrum? Quas nisi sit molestias ipsa atque cupiditate sapiente esse libero, repellat ea officiis obcaecati eos quos incidunt, adipisci porro est quod qui? Incidunt, molestiae assumenda! Ab."
+    },
+    {
+        question : "lorem2",
+        answer : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi ducimus fugiat impedit debitis, accusamus aperiam culpa adipisci temporibus excepturi, obcaecati consequuntur quos quibusdam optio iure ullam amet ipsum inventore rerum voluptate corrupti ipsa consectetur voluptas vero molestias! Placeat consequuntur voluptate excepturi, id veniam laudantium neque, distinctio porro et voluptatibus repellendus! Accusamus assumenda deleniti corporis. Ipsam quae amet illo numquam veritatis at fugiat incidunt mollitia, doloremque velit distinctio suscipit sequi laudantium minus repellat voluptatem nostrum? Quas nisi sit molestias ipsa atque cupiditate sapiente esse libero, repellat ea officiis obcaecati eos quos incidunt, adipisci porro est quod qui? Incidunt, molestiae assumenda! Ab."
+    },
+
+]
+
+export default data;
